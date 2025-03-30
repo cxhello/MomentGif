08089addfa89be0038777347795adfc13cbb8eb3
cxhello <caixiaohuichn@gmail.com>
2025-03-30 18:19:54 +0800

    Initial Commit

---
+++ MomentGif/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MomentGif
+//
+//  Created by cxhello on 2024/12/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MomentGif/MomentGifApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentGifApp.swift
+//  MomentGif
+//
+//  Created by cxhello on 2024/12/29.
+//
+
+import SwiftUI
+
+@main
+struct MomentGifApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
